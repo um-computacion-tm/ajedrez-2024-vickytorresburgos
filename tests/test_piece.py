67c47397b613a74713f391b35cc73312203b77ef
vicky <vicky@pop-os.localdomain>
2024-08-16 22:52:05 -0300

    adding piece testing

---
+++ tests/test_piece.py
@@ -0,0 +1,10 @@
+import unittest
+from game.piece import Piece
+
+class TestPiece(unittest.TestCase):
+    def test_SetUp(self):
+        piece = Piece("White")
+        self.assertEqual(piece.color,"White")
+
+if __name__ == "__main__":
+    unittest.main()
