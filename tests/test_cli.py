c61caca1c92dca9a550eb81f1f4cb2f54b650817
vicky <vicky@pop-os.localdomain>
2024-08-22 22:57:43 -0300

    adding input validation testing in class cli implementation

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,10 @@
 import unittest
 from unittest.mock import patch
 from game.chess import Chess
-from game.cli import play
+from game.cli import play, validate_input
     
 class TestCli(unittest.TestCase):
+
     @patch('builtins.input', side_effect = ['1','1','2','2'])
     @patch('builtins.print')
     @patch.object(Chess, 'move')
@@ -25,6 +26,29 @@ class TestCli(unittest.TestCase):
         self.assertEqual(mock_input.call_count, 1)
         self.assertEqual(mock_print.call_count, 2)
         self.assertEqual(mock_chess_move.call_count, 0)
+
+    def test_valid_input(self):
+        chess = Chess()
+        self.assertEqual(validate_input('5'), 5)
+        self.assertEqual(validate_input('0'), 0)
+        self.assertEqual(validate_input('8'), 8)
+
+    def test_invalid_range_input(self):
+        chess = Chess()
+        with self.assertRaises(ValueError):
+            validate_input('10')
+        with self.assertRaises(ValueError):
+            validate_input('-1')
+
+    def test_invalid_input_not_integer(self):
+        chess = Chess()
+        with self.assertRaises(ValueError):
+            validate_input('hola')
+        with self.assertRaises(ValueError):
+            validate_input('5.5')
+        with self.assertRaises(ValueError):
+            validate_input(' ')
+        
 
 if __name__ == '__main__':
     unittest.main()
