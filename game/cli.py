86bcbb71483876b5dbd860cfcc27d2b50f0bcad7
vicky <vicky@pop-os.localdomain>
2024-08-24 21:15:10 -0300

    fixed validate_input and tested cli

--- game/cli.py
+++ game/cli.py
@@ -1,36 +1,44 @@
 from game.chess import Chess
 
+class RangeError(Exception):
+    pass
+
 def main():
     chess = Chess()
-    while True:
+    while chess.is_playing():
         play(chess)
-
-def validate_input(value):
-    try:
-        number = int(value)
-        if number < 0 or number > 8:
-            raise ValueError
-    except ValueError:
-        raise ValueError("Invalid input. Please enter a number between 0 and 8.")
-    return number
 
 def play(chess):
     try:
         print("turn: ", chess.turn)
-        from_row = int(input('From row: ')) 
-        from_col = int(input('From col: ')) 
-        to_row = int(input('To row: ')) 
-        to_col = int(input('To col: ')) 
-  
+        from_row = int(input('From row: '))
+        validate_input(from_row)
+        
+        from_col = int(input('From col: '))
+        validate_input(from_col)
+
+        to_row = int(input('To row: '))
+        validate_input(to_row)
+
+        to_col = int(input('To col: '))
+        validate_input(to_col)
+
         chess.move(
             from_row,
             from_col,
             to_row,
             to_col,
         ) 
+    
+    except ValueError:
+        print('Unexpected input. Please try again')
+    
+    except RangeError:
+        print('Coords out of range. Please try again')
         
-    except ValueError as e:
-        print(f"Error de entrada: {e}")
-
+def validate_input(value):
+    if value < 1 or value > 8:
+        raise RangeError
+   
 if __name__ == '__main__':
     main()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,54 +1,79 @@
 import unittest
 from unittest.mock import patch
 from game.chess import Chess
-from game.cli import play, validate_input
+from game.cli import play, validate_input, RangeError
     
 class TestCli(unittest.TestCase):
-
-    @patch('builtins.input', side_effect = ['1','1','2','2'])
+    @patch('builtins.input',side_effect=['1', '1', '2', '2'])
     @patch('builtins.print')
     @patch.object(Chess, 'move')
+    @patch('game.cli.validate_input')
 
-    def test_happy_path(self,mock_chess_move,mock_print,mock_input):
+    def test_happy_path(self,mock_validate_input,mock_chess_move,mock_print,mock_input):
         chess = Chess()
         play(chess)
-        self.assertEqual(mock_input.call_count,4)
-        self.assertEqual(mock_print.call_count,1)
-        self.assertEqual(mock_chess_move.call_count,1)
+        self.assertEqual(mock_validate_input.call_count, 4)
+        self.assertEqual(mock_chess_move.call_count, 1)
+        self.assertEqual(mock_print.call_count, 1)
+        self.assertEqual(mock_input.call_count, 4)
 
-    @patch('builtins.input', side_effect = ['hola','1','2','2'])
-    @patch('builtins.print') # este patch controla lo que hace el print
+
+    @patch('builtins.input',side_effect=['hola', '1', '2', '2'])
+    @patch('builtins.print') 
     @patch.object(Chess, 'move')
+    @patch('game.cli.validate_input')
 
-    def test_not_happy_path(self,mock_chess_move,mock_print,mock_input): 
+    def test_not_happy_path(self,mock_validate_input,mock_chess_move,mock_print,mock_input):
+        chess = Chess()
+        play(chess)
+        self.assertEqual(mock_validate_input.call_count, 0)
+        self.assertEqual(mock_chess_move.call_count, 0)
+        self.assertEqual(mock_print.call_count, 2)
+        self.assertEqual(mock_input.call_count, 1)
+
+
+    @patch('builtins.input',side_effect=['1', '1', '2', 'hola'])
+    @patch('builtins.print') 
+    @patch.object(Chess, 'move')
+    @patch('game.cli.validate_input')
+
+
+    def test_more_not_happy_path(self,mock_validate_input,mock_chess_move,mock_print,mock_input):
+        chess = Chess()
+        play(chess)
+        self.assertEqual(mock_input.call_count, 4)
+        self.assertEqual(mock_print.call_count, 2)
+        self.assertEqual(mock_chess_move.call_count, 0)
+        self.assertEqual(mock_validate_input.call_count, 3)
+    
+
+    @patch('builtins.input',side_effect=['10', '1', '2', '1'])
+    @patch('builtins.print') 
+    @patch.object(Chess, 'move')
+    @patch('game.cli.validate_input', side_effect = [RangeError])
+
+    def test_range_error(self,mock_validate_input,mock_chess_move,mock_print,mock_input):
         chess = Chess()
         play(chess)
         self.assertEqual(mock_input.call_count, 1)
         self.assertEqual(mock_print.call_count, 2)
         self.assertEqual(mock_chess_move.call_count, 0)
+        self.assertEqual(mock_validate_input.call_count, 1)
+  
 
     def test_valid_input(self):
         chess = Chess()
-        self.assertEqual(validate_input('5'), 5)
-        self.assertEqual(validate_input('0'), 0)
-        self.assertEqual(validate_input('8'), 8)
+        self.assertEqual(validate_input(1), None)
+        self.assertEqual(validate_input(5), None)
+        self.assertEqual(validate_input(8), None)
 
     def test_invalid_range_input(self):
         chess = Chess()
-        with self.assertRaises(ValueError):
-            validate_input('10')
-        with self.assertRaises(ValueError):
-            validate_input('-1')
+        with self.assertRaises(RangeError):
+            validate_input(10)
+        with self.assertRaises(RangeError):
+            validate_input(-1)
 
-    def test_invalid_input_not_integer(self):
-        chess = Chess()
-        with self.assertRaises(ValueError):
-            validate_input('hola')
-        with self.assertRaises(ValueError):
-            validate_input('5.5')
-        with self.assertRaises(ValueError):
-            validate_input(' ')
-        
 
 if __name__ == '__main__':
     unittest.main()
