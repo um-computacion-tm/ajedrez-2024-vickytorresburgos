f049d40218246340a5b8b0482274d309358dcf10
vicky <vicky@pop-os.localdomain>
2024-08-26 22:31:45 -0300

    fixing piece testing

--- tests/test_piece.py
+++ tests/test_piece.py
@@ -4,7 +4,7 @@ from game.piece import Piece
 class TestPiece(unittest.TestCase):
     def test_SetUp(self):
         piece = Piece("White")
-        self.assertEqual(piece.color,"White")
+        self.assertEqual(piece.__color__,"White")
 
 if __name__ == "__main__":
     unittest.main()
