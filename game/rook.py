42c62cd80b483fec468fd9ddc63b4927c27c2da3
vicky <vicky@pop-os.localdomain>
2024-08-29 12:36:12 -0300

    adding rook testing

--- game/rook.py
+++ game/rook.py
@@ -1,17 +1,28 @@
 from game.piece import Piece
 
 class Rook(Piece):
-    white_str = "♖"
-    black_str = "♜"
-    
-    def possible_positions_vd(self,row,col):
+    def white_str(self):
+        return "♖"
+
+    def black_str(self):
+        return "♜"
+
+    def possible_positions_vd(self, row, col):
         possibles = []
-        for next_row in range (row + 1, 8):
-            possibles.append((next_row,col))
+        for next_row in range(row + 1, 8):
+            # que la celda que sigue no este ocupada..
+            other_piece = self.__board__.get_piece(next_row, col)
+            if other_piece is not None:
+                if other_piece.__color__ != self.__color__:
+                    possibles.append((next_row, col))
+                break
+            possibles.append((next_row, col))
         return possibles
 
-    def possible_positions_va(self,row,col):
+    def possible_positions_va(self, row, col):
         possibles = []
-        for next_row in range (row - 1, -1, -1):
-            possibles.append((next_row,col))
+        for next_row in range(row - 1, -1, -1):
+            possibles.append((next_row, col))
         return possibles
+
+
--- tests/test_rook.py
+++ tests/test_rook.py
@@ -1,22 +1,45 @@
 import unittest
 from game.board import Board
 from game.rook import Rook
+from game.pawn import Pawn
 
 class TestRook(unittest.TestCase):
 
-    # def test_str(self):
-    #     rook = Rook("White")
-    #     self.assertEqual(str(rook),"♜")
+    def test_str_white(self):
+        rook = Rook('White', self)
+        self.assertEqual(rook.white_str(), '♖')
 
-    def test_move_vertical_desc(self):
-        rook = Rook("White")
-        possibles = rook.possible_positions_vd(4,0)
-        self.assertEqual(possibles,[(5,0),(6,0),(7,0)])
+    def test_black_str(self):
+        rook = Rook('Black', self)
+        self.assertEqual(rook.black_str(), '♜')
+
+    # def test_move_vertical_desc(self):
+    #     board = Board()
+    #     rook = Rook("White", board)
+    #     possibles = rook.possible_positions_vd(4,1)
+    #     self.assertEqual(possibles,[(5,1),(6,1),(7,1)])
 
     def test_move_vertical_asc(self):
-        rook = Rook("White")
-        possibles = rook.possible_positions_va(4,0)
-        self.assertEqual(possibles,[(3,0),(2,0),(1,0),(0,0)])
+        board = Board()
+        rook = Rook("White", board)
+        possibles = rook.possible_positions_va(4, 1)
+        self.assertEqual(possibles,[(3, 1), (2, 1), (1, 1), (0, 1)])
+
+    def test_move_vertical_desc_with_own_piece(self):
+        board = Board()
+        board.place_piece(6, 1, Pawn("White", board))
+        rook = Rook("White", board)
+        board.place_piece(4, 1, rook)
+        possibles = rook.possible_positions_vd(4, 1)
+        self.assertEqual(possibles,[(5, 1)])
+
+    def test_move_vertical_desc_with_not_own_piece(self):
+        board = Board()
+        board.place_piece(6, 1, Pawn("BLACK", board))
+        rook = Rook("White", board)
+        board.place_piece(4, 1, rook)
+        possibles = rook.possible_positions_vd(4, 1)
+        self.assertEqual(possibles,[(5, 1), (6, 1)])
 
 if __name__ == "__main__":
     unittest.main()
