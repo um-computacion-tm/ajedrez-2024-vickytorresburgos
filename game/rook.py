8f4c3f593dda9f806e21354b1e2253ea16d5d2ac
vicky <vicky@pop-os.localdomain>
2024-08-30 22:49:47 -0300

    adding horizontal movement

--- game/rook.py
+++ game/rook.py
@@ -21,7 +21,37 @@ class Rook(Piece):
     def possible_positions_va(self, row, col):
         possibles = []
         for next_row in range(row - 1, -1, -1):
+            other_piece = self.__board__.get_piece(next_row, col)
+            if other_piece is not None:
+                if other_piece.__color__ != self.__color__:
+                    possibles.append((next_row, col))
+                break
             possibles.append((next_row, col))
         return possibles
+    
+    def possible_positions_hl(self,row,col):
+        possibles = []
+        for next_col in range(col + 1, 8):
+            other_piece = self.__board__.get_piece(row, next_col)
+            if other_piece is not None:
+                if other_piece.__color__ != self.__color__:
+                    possibles.append((row, next_col))
+                    break
+                possibles.append((row, next_col))
+            return possibles
+    
+    def possible_positions_hr(self,row,col):
+        possibles = []
+        for next_col in range(col - 1, -1, -1):
+            other_piece = self.__board__.get_piece(row, next_col)
+            if other_piece is not None:
+                if other_piece.__color__ != self.__color__:
+                    possibles.append((row, next_col))
+                    break
+                possibles.append((row, next_col))
+            return possibles
+    
+
+
     
 
