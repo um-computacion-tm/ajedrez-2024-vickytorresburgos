6e308dd9d35e90c60cc11152659c76af91ab6f1b
vicky <vicky@pop-os.localdomain>
2024-09-02 19:54:48 -0300

    adding piece testing

--- tests/test_piece.py
+++ tests/test_piece.py
@@ -1,11 +1,18 @@
 import unittest
 from game.piece import Piece
-from game.board import Board
 
 class TestPiece(unittest.TestCase):
     def test_SetUp(self):
         piece = Piece("White",self)
         self.assertEqual(piece.__color__,"White")
+
+    def test_white_str(self):
+        piece = Piece("White",self)
+        self.assertEqual(str(piece),"")
+
+    def test_black_str(self):
+        piece = Piece("Black",self)
+        self.assertEqual(str(piece),"")
 
 if __name__ == "__main__":
     unittest.main()
