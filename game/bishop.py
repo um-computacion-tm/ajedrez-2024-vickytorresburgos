b9e714271e371553c6d477dc4a4582a4c951c276
vicky <vicky@pop-os.localdomain>
2024-09-03 09:57:08 -0300

    adding bishop movements and testing

--- game/bishop.py
+++ game/bishop.py
@@ -6,3 +6,23 @@ class Bishop(Piece):
 
     def black_str(self):
         return "♝"
+    
+    def possible_positions(self, row, col):
+        possibles = []
+
+        # Direcciones diagonales: (up_right, up_left, down_right, down_left)
+        directions = [(-1, 1), (-1, -1), (1, 1), (1, -1)]
+
+        for dr, dc in directions:
+            next_row, next_col = row + dr, col + dc
+            while 0 <= next_row < 8 and 0 <= next_col < 8:
+                other_piece = self.__board__.get_piece(next_row, next_col)
+                if other_piece is not None:
+                    if other_piece.__color__ != self.__color__:
+                        possibles.append((next_row, next_col))
+                    break
+                possibles.append((next_row, next_col))
+                next_row += dr
+                next_col += dc
+
+        return possibles
