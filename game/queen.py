467f1f4c2b01feaa3ccb6527d4785fa8cb2ead46
vicky <vicky@pop-os.localdomain>
2024-10-13 20:54:44 -0300

    fixing2

--- game/queen.py
+++ game/queen.py
@@ -1,6 +1,12 @@
 from game.piece import Piece
 
 class Queen(Piece):
+
+    def __init__(self, color, board, score):
+        super().__init__(color, board, score)
+        self.__directions__ = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, 1), (-1, -1), (1, 1), (1, -1)]
+
+
     def white_str(self):
         # """
         # Returns the Unicode character for a white queen.
--- game/rook.py
+++ game/rook.py
@@ -1,6 +1,12 @@
 from game.piece import Piece
 
 class Rook(Piece):
+
+    def __init__(self, color, board, score):
+        super().__init__(color, board, score)
+        self.__directions__ = [(-1, 0), (1, 0), (0, -1), (0, 1)] 
+
+
     def white_str(self):
         """
         Returns the Unicode character for a white rook.
@@ -19,30 +25,6 @@ class Rook(Piece):
         """
         return "♜"
 
-    # def possible_positions(self, row, col):
-    #     """
-    #     Calculates the possible positions the rook can move to.
-
-    #     Parameters:
-    #         row (int): The current row of the rook.
-    #         col (int): The current column of the rook.
-
-    #     Returns:
-    #         list: A list of tuples representing the possible positions the rook can move to.
-    #     """        
-    #     possibles = []
-    #     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)] 
-
-    #     for dr, dc in directions:
-    #         next_row, next_col = row + dr, col + dc
-    #         while 0 <= next_row < 8 and 0 <= next_col < 8:
-    #             possibles.append((next_row, next_col))
-    #             next_row += dr
-    #             next_col += dc
-    #     return possibles
-    
-    def get_directions(self):
-        return [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 
     
