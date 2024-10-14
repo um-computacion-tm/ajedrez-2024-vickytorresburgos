788c13242152da817e2b31c7c6f851409dde506d
vicky <vicky@pop-os.localdomain>
2024-10-13 21:13:12 -0300

    fixing

--- game/bishop.py
+++ game/bishop.py
@@ -2,21 +2,20 @@ from game.piece import Piece
 
 class Bishop(Piece):
     def white_str(self):
-        """
-        Returns the Unicode character for a white bishop.
+        # """
+        # Returns the Unicode character for a white bishop.
 
-        Returns:
-            str: The Unicode character for a white bishop.
-        """
+        # Returns:
+        #     str: The Unicode character for a white bishop.
+        # """
         return "♗"
 
     def black_str(self):
-        """
-        Returns the Unicode character for a black bishop.
+        # """
+        # Returns the Unicode character for a black bishop.
 
-        Returns:
-            str: The Unicode character for a black bishop.
-        """
-
+        # Returns:
+        #     str: The Unicode character for a black bishop.
+        # """
         return "♝"
     
--- game/rook.py
+++ game/rook.py
@@ -2,21 +2,21 @@ from game.piece import Piece
 
 class Rook(Piece):
     def white_str(self):
-        """
-        Returns the Unicode character for a white rook.
+        # """
+        # Returns the Unicode character for a white rook.
 
-        Returns:
-            str: The Unicode character for a white rook.
-        """
+        # Returns:
+        #     str: The Unicode character for a white rook.
+        # """
         return "♖"
 
     def black_str(self):
-        """
-        Returns the Unicode character for a black rook.
+        # """
+        # Returns the Unicode character for a black rook.
 
-        Returns:
-            str: The Unicode character for a black rook.
-        """
+        # Returns:
+        #     str: The Unicode character for a black rook.
+        # """
         return "♜"
 
 
