a820ef3b4bcb22b5c38163a8a239771c259099e2
vicky <vicky@pop-os.localdomain>
2024-10-13 21:08:42 -0300

    fix

--- game/bishop.py
+++ game/bishop.py
@@ -1,12 +1,6 @@
 from game.piece import Piece
 
 class Bishop(Piece):
-
-    def __init__(self, color, board, score):
-        super().__init__(color, board, score)
-        self.__directions__ = [(-1, 1), (-1, -1), (1, 1), (1, -1)]
-
-
     def white_str(self):
         """
         Returns the Unicode character for a white bishop.
